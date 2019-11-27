2dfaa0d8109bdf2d70a2c5a14538e6a012ca4272
it62070068 <56394436+it62070068@users.noreply.github.com>
2019-11-28 00:10:58 +0700

    Update find_pair.py
    เพิ่มฟังก์ชั่น เเสดงที่นั่งทั้งหมด

--- find_pair.py
+++ find_pair.py
@@ -1,23 +1,36 @@
 '''pair location searcher'''
 
-dic_location = {i+j:'default' for i in 'ABCDEFGH' for j in '123456789'}
+dic_location = {j+i:'default' for i in '123456789' for j in 'ABCDEFGH'}
 
 def main(user_choice):
     '''main function for finding pair location'''
     print('--------------------------------------')
     if user_choice == 'Find':
-        find_pair(input('your pair_ID : '))
+        choose_other_choices(input('if you want to find only your pair says "Pair" if you want to see all location says "All" : '))
         print('--------------------------------------')
     elif user_choice == 'Tell':
         upload_your_location()
         print('save your position complete')
         print('--------------------------------------')
     else:
-        print('ERROR please try again only "F" or "T"')
+        print('ERROR please try again only "Find" or "Tell"')
         print('--------------------------------------')
     main(input('if you want to find pair says "Find" but if you want to tell your positon says "Tell" : '))
 
+def choose_other_choices(user_choice):
+    if user_choice == 'Pair':
+        print('--------------------------------------')
+        find_pair(input('your pair_ID : '))
+    elif user_choice == 'All':
+        print('--------------------------------------')
+        show_all_location()
+    else:
+        print('ERROR please try again only "F" or "T"')
+        print('--------------------------------------')
+        choose_other_choices(input('if you want to find only your pair says "Pair" if you want to see all location says "All" : '))
+
 def find_pair(pair_id):
+    print('--------------------------------------')
     if check_id_in_dic(pair_id) == True:
         pair_location = search_from_dic(pair_id)
         print(pair_location)
@@ -57,6 +70,22 @@ def filter_location(your_position, dic=dic_location):
     print('ERROR please tell your positon again')
     print('--------------------------------------')
     return filter_location(input('Your Positon from A1 - A9, B1 - B9, .... H1 - H9 : '))
+
+def show_all_location():
+    print()
+    all_row_left = range(0, 65, 8)
+    for row_left in all_row_left:
+        show_row(row_left)
+        print('\n')
+
+def show_row(row_left):
+    row_right = row_left + 7
+    row = list(dic_location.items())[row_left:row_right+1]
+    for location, stu_id in row:
+        if stu_id == 'default':
+            print('   ' + location, end='         ')
+        else:
+            print(stu_id, end='      ')
 
 def test():
     dic_test = {'A1':'62070068', 'A2':'default', 'A3':'62070038'}
